3f496c00dbd78b7a24d6b987a03c9221dae7d7f2
高佳圆 <2016011251@secoder.net>
2018-11-28 21:18:07 +0800

    [change]reg的名字

---
+++ cpu_project/reg.vhd
@@ -0,0 +1,29 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity reg is
+	port(
+	SP_before, IH_before : in std_logic_vector(15 downto 0);
+	reg_before : in std_logic_vector(127 downto 0);
+	PC0 : in std_logic_vector(15 downto 0);
+	Instruction : in std_logic_vector(15 downto 0);
+	Target : in std_logic_vector(3 downto 0);
+	Data : in std_logic_vector(15 downto 0);
+
+	SP_after, IH_after : out std_logic_vector(15 downto 0);
+	reg_after : out std_logic_vector(127 downto 0);
+	Rx, Ry, Rz : out std_logic_vector(15 downto 0);
+	Index : out std_logic_vector(11 downto 0);
+	led : out std_logic_vector(15 downto 0)
+);
+end reg;
+
+architecture bhv of reg is
+
+begin
+	process()
+		end process;
+
+	end bhv;
