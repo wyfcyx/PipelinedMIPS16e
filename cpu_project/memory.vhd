b4ad7dde8c0ee0c0d64533c2fe5336d9c06ea1fb
吴一凡 <2016011269@secoder.net>
2018-12-04 22:11:22 +0800

    12/4 22:11

--- cpu_project/memory.vhd
+++ cpu_project/memory.vhd
@@ -107,7 +107,7 @@ begin
 				end if;
 				case r2State is
 					when waiting =>
-						if ((LFlag = '1' or SFlag = '1') and Address < x"4000") then
+						if ((LFlag = '1' or SFlag = '1') and (Address < x"4000")) then
 							if (LFlag = '1') then
 								r2State <= readMem1;
 							else
@@ -157,7 +157,7 @@ begin
 				end case;
 				case r1State is
 					when waiting =>
-						if ((LFlag = '1' or SFlag = '1') and Address < x"4000") then
+						if ((LFlag = '1' or SFlag = '1') and (Address < x"4000")) then
 							r1State <= done;
 						elsif (LFlag = '1') then
 							if (Address = x"bf00") then
