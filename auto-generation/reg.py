e8f79b30ed4757b87ade58c7206e09bb0a51aae2
高佳圆 <2016011251@secoder.net>
2018-12-07 03:51:33 +0800

    [asads]

--- auto-generation/reg.py
+++ auto-generation/reg.py
@@ -16,10 +16,7 @@ reg={
         '11101', '11100', '11011', '01100', '11010'
     ],
     'special':[
-        'LW_SP','MFIH','MFPC','SW_SP'
-    ],
-    'sindex':[
-        9,8,10
+        'LW_SP','SW_SP'
     ],
     'sbit15-11':[
         '1001','1000','1010'
@@ -55,41 +52,55 @@ def writereg(f):
 def Rz(index,f):
     #判断是不是特殊情况
     f.write('''
-                flag2 <= "000"; --初始化
-            ''')
-    for i in range(len(reg['special'])):
-        #如果是特殊情况
-        cur=reg['instruction'].index(reg['special'][i])
-        f.write('''
-                if (Instruction(15 downto 11) = "%s") then
-                    flag1 <= '1'; --存在特殊情况
-                    flag2 <= "%s"; --临时标记
-                end if;
-                '''%(reg['bit15-11'][cur],'{:03b}'.format(i+1)))
-    f.write('''
-            if flag2 = "000" then --正常情况
-                Rz <= reg_before(%d downto %d);
-                index(11 downto 8) <= "%s";
-            elsif  flag2 = "001" then
-                Rz <= SP_before;
-                index(11 downto 8) <= "%s";
-                flag3 <= "1001";
-            elsif flag2 = "010" then
-                Rz <= IH_before;
-                index(11 downto 8) <= "%s";
-                flag3 <= "1000";
-            elsif flag2 = "011" then
-                Rz<= PC0;
-                index(11 downto 8) <= "%s";
-                flag3 <= "1010";
-            else
-                Rz <= SP_before;
-                index(11 downto 8) <= "%s";
-                flag3 <= "1001";
-            end if;
-            '''%(
-                index*16+15,index*16,'{:04b}'.format(index),reg['sbit15-11'][0],reg['sbit15-11'][1],reg['sbit15-11'][2],reg['sbit15-11'][0]
+                if (Instruction(15 downto 11) = "10010") then
+                    --LW_SP
+                    if Target(3 downto 0) = "1001" then
+                        Rz(15 downto 0) <= Data(15 downto 0);
+                    else
+                        Rz(15 downto 0) <= SP_before(15 downto 0);
+                    end if;
+                    index(11 downto 8) <= "1001";
+                elsif (Instruction(15 downto 11) = "11010") then
+                    --SW_SP
+                    if Target(3 downto 0) = "1001" then
+                        Rz(15 downto 0) <= Data(15 downto 0);
+                    else
+                        Rz(15 downto 0) <= SP_before(15 downto 0);
+                    end if;
+                    index(11 downto 8) <= "1001";
+                elsif (Instruction(15 downto 11) = "11110") then
+                    if (Instruction(7 downto 0) = "00000000") then
+                        --MFIH
+                        if Target(3 downto 0) = "1000" then
+                            Rz(15 downto 0) <= Data(15 downto 0);
+                        else
+                            Rz <= IH_before;
+                        end if;
+                        index(11 downto 8) <= "1000";
+                    end if;
+                elsif (Instruction(15 downto 11) = "11101") then
+                    if (Instruction(7 downto 0) = "01000000") then
+                        --MFPC
+                        if Target(3 downto 0) = "1010" then
+                            Rz(15 downto 0) <= Data(15 downto 0);
+                        else
+                            Rz(15 downto 0) <= PC0(15 downto 0);
+                        end if;
+                        index(11 downto 8) <= "1010";
+                    end if;
+                else
+                    --normal
+                    if Target(3 downto 0) = "%s" then
+                        Rz <= Data(15 downto 0);
+                    else
+                        Rz <= reg_before(%d downto %d);
+                    end if;
+                    index(11 downto 8) <= "%s";'''%(
+    '{:04b}'.format(index),index*16+15,index*16,'{:04b}'.format(index)
             ))
+    f.write('''
+                end if;
+            ''')
 def R(index,f):
     cur=['z','y','x']
     for dataIndex in range(8):
@@ -101,10 +112,14 @@ def R(index,f):
         ))
         if index !=0:
             f.write('''
-            R%s <= reg_before(%d downto %d);
+            if Target(3 downto 0) = "%s" then
+                R%s <= Data(15 downto 0);
+            else
+                R%s <= reg_before(%d downto %d);
+            end if;
             Index(%d downto %d) <=  "%s";
                     '''%(
-                        cur[index],dataIndex*16+15,dataIndex*16,4*(2-index)+3,4*(2-index),'{:04b}'.format(dataIndex)
+                        '{:04b}'.format(dataIndex),cur[index],cur[index],dataIndex*16+15,dataIndex*16,4*(2-index)+3,4*(2-index),'{:04b}'.format(dataIndex)
             )
                    )
         else:
@@ -139,45 +154,18 @@ entity reg is
 	);
 end reg;
 architecture bhv of reg is
-    signal flag1: std_logic; --说明是否存在特殊情况
-    signal flag2: std_logic_vector(2 downto 0); --临时变量
-    signal flag3: std_logic_vector(3 downto 0); --特殊寄存器
 begin
 process(SP_before,IH_before,reg_before,PC0,Instruction,Target,Data)
 begin
-    flag1 <= '0';
     ''')
     #添加中间代码
     for i in range(3):
         R(i,f);
     f.write('''
         if Target(3 downto 0) < 15 then
-            -- 不是输出
             led(15 downto 0)<=Data(15 downto 0);
         else 
             led(15 downto 0)<= (others=>'0');
-        end if;
-
-        if Target(3) = '0' then
-            if Target(2 downto 0) = Instruction(10 downto 8) then
-                Rx(15 downto 0) <= Data(15 downto 0);
-            end if;
-
-            if Target(2 downto 0) = Instruction(7 downto 5) then
-                Ry(15 downto 0) <= Data(15 downto 0);
-            end if;
-
-            if flag1 = '0' then --不存在特殊情况
-                if Target(2 downto 0) = Instruction(4 downto 2) then
-                    Rz(15 downto 0) <= Data(15 downto 0);
-                end if;
-            end if;
-        else
-            if flag1 = '1' then --存在特殊情况
-                if Target(3 downto 0) = flag3(3 downto 0) then
-                    Rz(15 downto 0)<= Data(15 downto 0);
-                end if;
-            end if;
         end if;
 
             ''')
